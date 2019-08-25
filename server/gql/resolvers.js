875cb9d45cb4d06ea0a72495b9ffd89f21751f54
yassine belkaid <yassine.belkaid87@gmail.com>
2019-08-25 23:39:58 +0100

    [server] added products resolver

--- server/gql/resolvers.js
+++ server/gql/resolvers.js
@@ -1,10 +1,20 @@
 import CategoryModel from '../models/category';
+import ProductModel from '../models/product';
+import { productsPresenter } from '../utils/presenter';
 
 const Query = {
     categories: async () => {
-        const data = await CategoryModel.find({}).sort([['createdAt', -1]]);
+        const categories = await CategoryModel.find({}).sort([
+            ['createdAt', -1],
+        ]);
 
-        return data;
+        return categories;
+    },
+    products: async () => {
+        const products = await ProductModel.find({}).sort([['createdAt', -1]]);
+        const updatedProducts = productsPresenter(products);
+
+        return updatedProducts;
     },
 };
 
