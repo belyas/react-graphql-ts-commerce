24711b2f79bf6c483ea1a7b7e0dfeb13b1073b68
yassine belkaid <yassine.belkaid87@gmail.com>
2019-08-25 23:11:26 +0100

    [client] added graphql categories to Menu

--- client/src/containers/Menu/Menu.js
+++ client/src/containers/Menu/Menu.js
@@ -1,37 +1,19 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import PropTypes from 'prop-types';
+import React from 'react';
+import { Query } from 'react-apollo';
 
-import { fetchCategories } from '../../store/actions';
+import { GET_CATEGORIES } from '../../gql/queries';
 import MenuComponent from '../../components/Menu/Menu';
 
-const Menu = ({ fetchCategories, loading, categories }) => {
-    useEffect(() => {
-        fetchCategories();
-        // eslint-disable-next-line
-    }, []);
+const Menu = () => (
+    <Query query={GET_CATEGORIES}>
+        {({ data: { categories }, loading, error }) => {
+            if (error) {
+                throw new Error(error);
+            }
 
-    const props = { loading, categories };
-    return <MenuComponent {...props} />;
-};
+            return <MenuComponent loading={loading} categories={categories} />;
+        }}
+    </Query>
+);
 
-Menu.propTypes = {
-    loading: PropTypes.bool.isRequired,
-    categories: PropTypes.array.isRequired,
-    fetchCategories: PropTypes.func.isRequired,
-};
-
-const mapStateToProps = state => ({
-    categories: state.category.categories,
-    loading: state.category.loading,
-});
-
-const mapDispatchToProps = dispatch => {
-    return bindActionCreators({ fetchCategories }, dispatch);
-};
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(Menu);
+export default Menu;
