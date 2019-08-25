4a8feaf00e024f4e096bb66ab52dfa712338bb01
yassine belkaid <yassine.belkaid87@gmail.com>
2019-08-25 23:42:00 +0100

    [server] removed products rest api from products routes

--- server/routes/api/product.js
+++ server/routes/api/product.js
@@ -3,7 +3,7 @@ import { productController } from '../../controllers/api';
 
 const router = express.Router();
 
-router.get('/', productController.all);
+// router.get('/', productController.all);
 router.get('/category/:categoryId', productController.getProductsByCategoryId);
 router.get('/:productId/product', productController.getProductId);
 
