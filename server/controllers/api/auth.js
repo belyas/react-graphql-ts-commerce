7ece9784fabd73854d26c69b8d44594a34817819
yassine belkaid <yassine.belkaid87@gmail.com>
2019-10-16 11:44:01 +0100

    [server] removed authlogin from Auth controller api

--- server/controllers/api/auth.js
+++ server/controllers/api/auth.js
@@ -5,41 +5,6 @@ import validator from 'validator';
 import UserModel from '../../models/user';
 
 export default class Auth {
-    static async login(req, res) {
-        const { email, password } = req.body;
-
-        try {
-            const user = await UserModel.findOne({ email });
-
-            if (!user) {
-                return res
-                    .status(401)
-                    .json({ error: 'Something went wrong.', user });
-            }
-
-            const isEqual = await bcrypt.compare(password, user.password);
-
-            if (!isEqual) {
-                return res.status(401).json({ error: 'Something went wrong.' });
-            }
-
-            const token = jwt.sign(
-                {
-                    firstname: user.firstname,
-                    lastname: user.lastname,
-                    email: user.email,
-                    userId: user._id.toString(),
-                },
-                process.env.JWT_SECRET,
-                { expiresIn: '1d' }
-            );
-
-            res.status(200).json({ token, userId: user._id.toString() });
-        } catch (err) {
-            res.status(500).json({ error: err.message });
-        }
-    }
-
     static checkUserStatus(req, res) {
         res.status(200).json({ success: true });
     }
