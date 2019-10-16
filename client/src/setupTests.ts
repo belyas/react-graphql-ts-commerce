e60a36c4c33ccfa6f1bec53ed450da7b97a0757c
yassine belkaid <yassine.belkaid87@gmail.com>
2019-10-16 09:17:37 +0100

    fixed global localstorage typescript typing

--- client/src/setupTests.ts
+++ client/src/setupTests.ts
@@ -3,11 +3,21 @@ import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
-const localStorageMock = {
-    getItem: jest.fn(),
-    setItem: jest.fn(),
-    removeItem: jest.fn(),
-    clear: jest.fn(),
+const localStorageMock: Storage = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn(),
+  length: 0,
+  key: jest.fn(),
 };
+
+export interface Global {
+  document: Document;
+  window: Window;
+  localStorage: Storage;
+}
+
+declare var global: Global;
 
 global.localStorage = localStorageMock;
