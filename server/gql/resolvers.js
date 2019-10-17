5ecdfc8f1155d70577823c57fe32e242d7854aaa
yassine belkaid <yassine.belkaid87@gmail.com>
2019-10-17 17:59:39 +0100

    [server] created auth signup mutation resolved

--- server/gql/resolvers.js
+++ server/gql/resolvers.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
+import validator from 'validator';
 
 import CategoryModel from '../models/category';
 import ProductModel from '../models/product';
@@ -51,6 +52,85 @@ const Mutation = {
             return { token, userId: user._id.toString() };
         } catch (err) {
             return { error: err.message };
+        }
+    },
+    authSignup: async (_, { firstname, lastname, email, password }) => {
+        const response = { success: false };
+
+        try {
+            const user = await UserModel.findOne({ email });
+
+            if (user) {
+                return {
+                    ...response,
+                    error: 'You cannot create this account!',
+                };
+            }
+
+            const lengthRange = { min: 3, max: 30 };
+            // validate fields
+            if (
+                validator.isEmpty(firstname) ||
+                !validator.isAlpha(firstname) ||
+                !validator.isLength(firstname, lengthRange)
+            ) {
+                return {
+                    ...response,
+                    error: 'Invalid first name!',
+                };
+            }
+
+            if (
+                validator.isEmpty(lastname) ||
+                !validator.isAlpha(lastname) ||
+                !validator.isLength(lastname, lengthRange)
+            ) {
+                return { ...response, error: 'Invalid last name!' };
+            }
+
+            if (!validator.isEmail(email) || validator.isEmpty(email)) {
+                return { ...response, error: 'Invalid email address!' };
+            }
+
+            if (
+                validator.isEmpty(password) ||
+                !password.match(/^[a-zA-Z0-9]{3,30}$/)
+            ) {
+                return {
+                    ...response,
+                    error:
+                        'Invalid password, please use only letters and numbers!',
+                };
+            }
+
+            try {
+                const genSalt = await bcrypt.genSalt(10);
+                const hash = await bcrypt.hash(password, genSalt);
+                const userObj = new UserModel({
+                    firstname,
+                    lastname,
+                    email,
+                    password: hash,
+                });
+
+                const savedUser = await userObj.save();
+
+                if (savedUser) {
+                    return { success: true, error: '' };
+                }
+
+                return {
+                    ...response,
+                    error: 'Ooops! could not save your data!',
+                };
+            } catch (err) {
+                return {
+                    ...response,
+                    error: err.message,
+                };
+            }
+        } catch (err) {
+            return { ...response, error: err.message };
         }
     },
 };
